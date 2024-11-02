e34c8c6a6493823b45a201b40c89c311e55cae47
Samuel Mimram <smimram@gmail.com>
2024-11-02 14:55:52 +0100

    Coefficient.

--- src/assignment.ml
+++ src/assignment.ml
@@ -29,22 +29,44 @@ module Question = struct
         | k -> warning "Unhandled key in question: %s" k
       ) yaml;
     { name = !name; grep = !grep; points = !points }
+
+  let points q = q.points
 end
 
 type t =
   {
     name : string;
+    points : float;
+    coefficient : float;
     questions : Question.t list;
   }
 
 let of_yaml yaml =
   let name = ref "" in
+  let points = ref 20. in
+  let coefficient = ref None in
   let questions = ref [] in
   let yaml = Yaml.to_obj_exn yaml in
   List.iter (fun (k,v) ->
       match k with
       | "name" -> name := Yaml.to_string_exn v
+      | "points" -> points := Yaml.to_float_exn v
+      | "coefficient" -> coefficient := Some (Yaml.to_float_exn v)
       | "questions" -> questions := List.map Question.of_yaml (Yaml.to_list_exn v)
       | k -> warning "Unhandled key: %s" k
     ) yaml;
-  { name = !name; questions = !questions }
+  let questions = !questions in
+  let points = !points in
+  let coefficient =
+    match !coefficient with
+    | Some c -> c
+    | None ->
+      let n = List.map Question.points questions |> List.fold_left (+.) 0. in
+      points /. n
+  in
+  {
+    name = !name;
+    points;
+    coefficient;
+    questions;
+  }
